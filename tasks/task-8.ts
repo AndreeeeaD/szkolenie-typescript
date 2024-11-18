eda032a020982f04851f7ad74cb877e35e18c882
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-11-18 14:33:48 +0100

    task 7-10

--- tasks/task-8.ts
+++ tasks/task-8.ts
@@ -1,3 +1,3 @@
 // napisz przeciężenie funkcji handleTask, które 
-// przyjmuje obiekt typu Task i zwraca string jeśli status jest 'new' lub 'in progress'
+// przyjmuje status zadania i zwraca string jeśli status jest 'new' lub 'in progress'
 // lub zwraca void jeśli status jest 'done'
